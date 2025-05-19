d502bcde2412cc85997ef20d368be8fe3a39fa94
nguyen <nguyenthcs430@gmail.com>
2025-05-19 21:23:27 +0700

    add libary

--- src/main/java/com/example/FindJobIT/FindJobItApplication.java
+++ src/main/java/com/example/FindJobIT/FindJobItApplication.java
@@ -3,7 +3,11 @@ package com.example.FindJobIT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class FindJobItApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/FindJobIT/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.FindJobIT.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test successful: ");
+    }
+}
