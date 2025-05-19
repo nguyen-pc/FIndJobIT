afd83d4c3215d9e3fd9c91b496cfb3d2a7033dbb
nguyen <nguyenthcs430@gmail.com>
2025-05-19 20:51:03 +0700

    init project

---
+++ src/main/java/com/example/FindJobIT/FindJobItApplication.java
@@ -0,0 +1,13 @@
+package com.example.FindJobIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindJobItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindJobItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FindJobIT/FindJobItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FindJobIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
