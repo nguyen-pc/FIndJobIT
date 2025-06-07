a95a782ad662c9fd392a3b15fbd12cc998a816e0
nguyen <nguyenthcs430@gmail.com>
2025-06-07 18:39:43 +0700

    implement exception

---
+++ src/main/java/com/example/FindJobIT/util/error/PermissionException.java
@@ -0,0 +1,8 @@
+package com.example.FindJobIT.util.error;
+
+public class PermissionException extends Exception {
+    public PermissionException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/main/java/com/example/FindJobIT/util/error/StorageException.java
@@ -0,0 +1,7 @@
+package com.example.FindJobIT.util.error;
+
+public class StorageException extends Exception{
+    public StorageException(String message) {
+        super(message);
+    }
+}
