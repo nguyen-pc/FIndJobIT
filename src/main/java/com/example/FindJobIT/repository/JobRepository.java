bfc0ceb257c9abe4022391246f155648a7a1bfcc
nguyen <nguyenthcs430@gmail.com>
2025-06-09 18:24:04 +0700

    build job service

--- src/main/java/com/example/FindJobIT/repository/JobRepository.java
+++ src/main/java/com/example/FindJobIT/repository/JobRepository.java
@@ -1,5 +1,17 @@
 package com.example.FindJobIT.repository;
 
-public class JobRepository {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.example.FindJobIT.domain.Job;
+import com.example.FindJobIT.domain.Skill;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long>,
+        JpaSpecificationExecutor<Job> {
+
+        List<Job> findBySkillsIn(List<Skill> skills);
 }
+
