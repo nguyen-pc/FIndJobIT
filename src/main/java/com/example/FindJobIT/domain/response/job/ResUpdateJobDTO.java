8c5c9bd80c91e4c561c35a981c978b3fa40c3d92
nguyen <nguyenthcs430@gmail.com>
2025-06-09 18:20:56 +0700

    build job controller

---
+++ src/main/java/com/example/FindJobIT/domain/response/job/ResUpdateJobDTO.java
@@ -0,0 +1,34 @@
+package com.example.FindJobIT.domain.response.job;
+
+
+import java.time.Instant;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import com.example.FindJobIT.util.constant.LevelEnum;
+
+@Getter
+@Setter
+public class ResUpdateJobDTO {
+    private long id;
+    private String name;
+
+    private String location;
+
+    private double salary;
+
+    private int quantity;
+
+    private LevelEnum level;
+
+    private Instant startDate;
+    private Instant endDate;
+    private boolean isActive;
+
+    private List<String> skills;
+
+    private Instant updatedAt;
+    private String updatedBy;
+}
+
