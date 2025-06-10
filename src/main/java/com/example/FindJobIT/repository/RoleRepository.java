29250633072c322576303a14de17c74242afc2fd
nguyen <nguyenthcs430@gmail.com>
2025-06-10 20:02:31 +0700

    create route role

--- src/main/java/com/example/FindJobIT/repository/RoleRepository.java
+++ src/main/java/com/example/FindJobIT/repository/RoleRepository.java
@@ -1,5 +1,14 @@
 package com.example.FindJobIT.repository;
 
-public class RoleRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import com.example.FindJobIT.domain.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>,
+                JpaSpecificationExecutor<Role> {
+        boolean existsByName(String name);
+
+        Role findByName(String name);
 }
