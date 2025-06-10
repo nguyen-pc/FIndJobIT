a54fea984d32d5089a8585d898803fe669888666
nguyen <nguyenthcs430@gmail.com>
2025-06-10 20:07:14 +0700

    build controller and service permission

--- src/main/java/com/example/FindJobIT/repository/PermissionRepository.java
+++ src/main/java/com/example/FindJobIT/repository/PermissionRepository.java
@@ -1,5 +1,15 @@
 package com.example.FindJobIT.repository;
 
-public class PermissionRepository {
-    
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import com.example.FindJobIT.domain.Permission;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long>,
+                JpaSpecificationExecutor<Permission> {
+        boolean existsByModuleAndApiPathAndMethod(String module, String apiPath, String method);
+
+        List<Permission> findByIdIn(List<Long> id);
 }
