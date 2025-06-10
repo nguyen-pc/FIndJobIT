8dae2c70070f66b466b608a3d7a8ae93d9d79931
nguyen <nguyenthcs430@gmail.com>
2025-06-10 20:23:24 +0700

    build controller and service resume

--- src/main/java/com/example/FindJobIT/repository/ResumeRepository.java
+++ src/main/java/com/example/FindJobIT/repository/ResumeRepository.java
@@ -1,5 +1,13 @@
 package com.example.FindJobIT.repository;
 
-public class ResumeRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.example.FindJobIT.domain.Resume;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, Long>,
+        JpaSpecificationExecutor<Resume> {
+
 }
